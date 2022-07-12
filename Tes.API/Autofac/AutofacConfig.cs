e52f0c9d90001760d1b08c9f3bc02cf7ec26fa2f
leibo <leibo@shjinjia.com.cn>
2022-07-12 10:56:48 +0800

    测试LB.Sqlsugar组件

---
+++ Tes.API/Autofac/AutofacConfig.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace Tes.API.Autofac
+{
+    public class AutofacConfig
+    {
+        public static void RegisteService()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+
+
+            //这一句必须要写,否则会出现Controller没有无参构造函数的错误 ......
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            
+
+            IContainer container = builder.Build();
+
+            // mvc 使用  DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
---
+++ Tes.API/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Tes.API.Autofac;
+
+namespace Tes.API
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //注册Autofac 
+            AutofacConfig.RegisteService();
+        }
+    }
+}
